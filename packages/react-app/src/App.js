cc7d1f8329c68029474c0974784e84c17fb2d1ba
julienbrg <julien@strat.cc>
2022-03-26 13:27:21 +0100

    add store

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -70,6 +70,52 @@ function App() {
     }
   }, [loading, subgraphQueryError, data]);
 
+  function getAccessToken() {
+    // Get your own API token at https://web3.storage/account/
+    return process.env.REACT_APP_WEB3STORAGE_TOKEN;
+    console.log("web3storage token: ", process.env.REACT_APP_WEB3STORAGE_TOKEN);
+
+  }
+  
+  function makeStorageClient() {
+    console.log("makeStorageClient ✅ ");
+    // return new Web3Storage({ token: getAccessToken() });
+    return new Web3Storage({ token: "<YOUR_OWN_WEBSTORAGE_TOKEN_GOES_HERE>" });
+  }
+
+  function makeFileObjects() {
+    console.log("makeFileObjects ✅ ");
+    // You can create File objects from a Blob of binary data
+    // see: https://developer.mozilla.org/en-US/docs/Web/API/Blob
+    // Here we're just storing a JSON object, but you can store images,
+    // audio, or whatever you want!
+    const obj = { game: 'Lode Runner' };
+    const blob = new Blob([JSON.stringify(obj)], {type : 'application/json'});
+  
+    const files = [
+      new File(['contents-of-file-1'], 'plain-utf8.txt'),
+      new File([blob], 'hello.json')
+    ];
+    return files;
+  }
+
+  async function storeFiles(files) {
+    // console.log("storeFiles ✅ ");
+    // const client = makeStorageClient();
+    // const cid = await client.put(files);
+    // console.log('stored files with cid:', cid);
+    // return cid;
+
+    console.log("storeFiles ✅ ");
+    console.log('worked ✅');
+  }
+
+  async function play() {
+    console.log("Hello! 👋 ");
+    makeStorageClient();
+    storeFiles(makeFileObjects());
+  }
+
   return (
     <Container>
       <Header>
@@ -78,8 +124,11 @@ function App() {
       <Body>
         <Image src={logo} alt="lode-runner" />
         <p></p>
-        <Button>
+        <Button onClick={play}>
           Mint
+        </Button>
+        <Button onClick={getAccessToken}>
+          Get access token
         </Button>
         <p></p>
         <Link href="https://github.com/julienbrg/spearmint">
